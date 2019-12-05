f513a22f16f1919717ed7e05b518eb299e08bcf0
Julia Kliuchuk <yulya.kliuchuk@gmail.com>
2019-12-06 02:07:13 +0300

    final version

--- src/virtual-scroll/test.js
+++ src/virtual-scroll/test.js
@@ -4,30 +4,33 @@ var scrollTop = 0;
 var methodType = 0;
 var containerInterval = [0, 0];
 var containerSelected = [];
-var scrollH = 0;
 var viewport, content, loadType;
-var stopLoad = false;
 
-/**
+ /**
  * Init JQuery script
  * @constructor
  * @return {[type]} [description]
  */
 $(function () {
-
   viewport = $("#viewport");
   content = $("#content");
   loadType = $("input[type=radio]");
 
-  // height of veiwport
-  vpHeight = viewport.height();
+   vpHeight = Math.floor(viewport.css("height").replace('px', ''));
+  // contentHeight = content.css("height").replace('px', '');
+  containerCount = $(".container").length;
 
-  loadType.change(selectMetodLoadContent);
+   // Sum height of elements
+  contentHeight = $(".container").toArray().reduce(function (sum, elem) {
+    return sum + Number($(elem).css("height").replace('px', ''));
+  }, 0);
+
+   loadType.change(selectMetodLoadContent);
   loadType.trigger("change");
 
-});
+ });
 
-/**
+ /**
  * Select Metod Load Content
  * @this $
  * @return {volid}
@@ -36,134 +39,114 @@ function selectMetodLoadContent() {
   if (this.checked) {
     methodType = this.value;
 
-    // kill all events
+     // kill all events
     viewport.unbind("mouseup");
     viewport.unbind("scroll");
 
-    // Loading the current viewport after positioning it
+     // Loading the current viewport after positioning it
     if (methodType == 1) {
-      getContainers(0, vpHeight);
       viewport.mouseup(function () {
         scrollTop = viewport.scrollTop();
-
         // initializing interval for search containers
         start = scrollTop;
         stop = vpHeight + scrollTop;
 
-        getContainers(start, stop);
+         getContainers(start, stop);
       });
       // run now
       viewport.trigger("mouseup");
 
-      // Anticipating viewport loading with a downward shift of the loading area by 1 viewport
+       // Anticipating viewport loading with a downward shift of the loading area by 1 viewport
     } else if (methodType == 2) {
 
-      viewport.scroll(function () {
-
+       viewport.scroll(function () {
         // Load first viewport
         getContainers(0, vpHeight);
         scrollTop = viewport.scrollTop();
-
         // initializing interval for search containers
         start = scrollTop + vpHeight;
         stop = vpHeight + start;
 
-        getContainers(start, stop);
+         getContainers(start, stop);
       });
-
       // run now
       viewport.trigger("scroll");
     }
   }
   logDebugInfo();
-};
+}
 
-/**
- * Получить контейнеры
+ /**
  * Get containers
  * @param  {Number} start
  * @param  {Number} stop
  * @return {volid}
  */
 function getContainers(start, stop) {
-  if(stopLoad) return;
-
+  var scrollH = 0;
   var result = [];
-  var countBlock = 1;
   containerInterval = [start, stop];
-  scrollH = 0;
-
   $(".container").toArray().find(function (elem) {
-
-    if ($(elem).attr('status') === '1') {
-
-      // height of one element
-      scrollH += $(elem).outerHeight(true);
-      countBlock++;
+    var elemH = Number($(elem).css("height").replace('px', ''));
+    scrollH += elemH;
+    // console.log(scrollH ,'>=', start)
+    // console.log(scrollH ,'>=', stop)
+    // console.log(elem.id)
+    if (scrollH >= start) {
+      result.push(elem);
+      if (scrollH >= stop) {
+        checkContainer(result);
+        return true;
+      }
     }
   });
-
-  if (scrollH < stop) {
-    loadFile('page-' + countBlock, $("#content"), getContainers.bind(null, start, stop));
-  }
-
   logDebugInfo();
 }
 
+ /**
+ * Checking loaded data of container
+ * @param  {Object} container element container
+ * @return {void}
+ */
+function checkContainer(container) {
+  containerSelected = container.map(function (elem) {
+    return elem.id;
+  });
 
+   container.forEach(function (elem) {
 
+     if ($(elem).attr('status') === '0') {
+      loadFile(elem.id, elem);
+    }
+  });
+  logDebugInfo();
+}
 
-/**
- * Загрузить файл в контейнер
+ /**
  * Load file to container
  * @param  {String} file      name of file
  * @param  {Object} container element container
  * @return {void}
  */
-function loadFile(file, container, cb) {
-
+function loadFile(file, container) {
   if (!file || !container) return;
-
-  var idBlock = '#' + file;
-  if ($(idBlock).length) return;
-
-  $(container).append('<div id=' + file + ' status="0" style ="margin-top:10px" class="container"></div>');
-
-  var newContainer = $(idBlock);
-  var countContainer = newContainer.length;
-
   $.ajax({
-    type: "GET",
+    type: "get",
     url: 'pages/' + file + '.html',
     dataType: "html",
     success: function (data) {
-      // handle data here
-      $(newContainer).append(data);
-
-      if($(newContainer).children().attr("status") === '-1'){
-        content.height(scrollH + 10);
-        stopLoad = true;
-        return;
-      }
-
-      $(newContainer).attr("status", '1');
-      var height = $(newContainer).children().height();
-
-      content.height(scrollH + height + vpHeight);
-
-      contentHeight = content.outerHeight(true);
-
-      $(newContainer).css("height", height);
-      cb();
+      /* handle data here */
+      $(container).html(data);
+      $(container).attr("status", '1');
     },
-    error: function (xhr) {
-      content.height(scrollH + 10);
+    error: function (xhr, status) {
+      /* handle error here */
+      $(container).html(status);
     }
   });
-
 }
 
-/**
+ /**
  * Log Debug information
  * @return {void}
  */
@@ -172,11 +155,19 @@ function logDebugInfo() {
   dbg.empty();
   dbg.append("vpHeight = " + vpHeight + "<br>");
   dbg.append("contentHeight = " + Math.round(contentHeight) + "<br>");
+  dbg.append("containerCount = " + Math.round(containerCount) + "<br>");
   dbg.append("MethodType = " + methodType + "<br>");
   dbg.append("<hr>");
   dbg.append("scrollTop = " + Math.round(scrollTop) + "<br>");
 
-  dbg.append("Container Interval:" + "<br>");
+   dbg.append("Container Interval:" + "<br>");
   dbg.append("  - start: " + Math.round(containerInterval[0]) + "<br>");
   dbg.append("  - stop : " + Math.round(containerInterval[1]) + "<br>");
+
+   dbg.append("<br>");
+  dbg.append("<hr>");
+  dbg.append("Container Selected:" + "<br>");
+  containerSelected.forEach(function (elem, i) {
+    dbg.append("  - index: " + i + "  name: \"" + elem + "\"<br>");
+  });
 }
